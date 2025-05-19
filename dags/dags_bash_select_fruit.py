573d0abb11ad942cb0d744ade748abe43af7fa75
kimsaru <kimsaru@gmail.com>
2025-05-19 15:32:33 +0900

    selectfruit.sh 수행하는 dag 업로드

---
+++ dags/dags_bash_select_fruit.py
@@ -0,0 +1,25 @@
+import datetime
+import pendulum
+
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="dags_bash_select_fruit",
+    schedule=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+) as dag:
+    
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="/opt/airflow/plugins/shell/test.sh test1",
+    )
+
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="/opt/airflow/plugins/shell/test.sh test2",
+    )
+
+    bash_t1 >> bash_t2
