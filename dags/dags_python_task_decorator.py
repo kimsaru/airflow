f301687ac72b7efb4c519734d7a67fca1b51dd40
kimsaru <kimsaru@gmail.com>
2025-05-19 21:16:36 +0900

    task decorator 테스트

---
+++ dags/dags_python_task_decorator.py
@@ -0,0 +1,20 @@
+import datetime
+import pendulum
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+import random
+from airflow.decorators import dag, task
+
+with DAG(
+    dag_id="dags_python_task_decorator",
+    schedule="0 2 * * 1",
+    start_date=pendulum.datetime(2025, 5, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    @task(task_id="print_task_1")
+    def print_context(some_input):
+        print(some_input)
+
+    print_task_1 = print_context('task_decorator 실행')
