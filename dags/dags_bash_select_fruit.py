3c35d3b94788ee6fcb6a2112a79fd531adca8542
kimsaru <kimsaru@gmail.com>
2025-05-26 18:47:44 +0900

    select fruit shell 수행하는 dag 업로드1

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -8,6 +8,7 @@ with DAG(
     dag_id="dags_bash_select_fruit",
     schedule=None,
     start_date=pendulum.datetime(2025, 5, 25, tz="Asia/Seoul"),
+    tags=["testtest1"],
     catchup=False,
 ) as dag:
     
