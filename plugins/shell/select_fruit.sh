41d568eb1461cf5ec1d073fdb13aa5f496a568ca
kimsaru <kimsaru@gmail.com>
2025-05-26 18:20:31 +0900

    conn 테스트 dag

--- plugins/shell/select_fruit.sh
+++ plugins/shell/select_fruit.sh
@@ -1,9 +1,9 @@
 FRUIT=$1
-if [ $FRUIT == APPLE];then
+if [ $FRUIT == APPLE ];then
     echo "You selected APPLE!"
-elif [ $FRUIT == ORANGE];then
+elif [ $FRUIT == ORANGE ];then
     echo "You selected ORANGE!"
-elif [ $FRUIT == GRAPE];then
+elif [ $FRUIT == GRAPE] ;then
     echo "You selected GRAPE!"
 else
     echo "You selected other Fruit!"
