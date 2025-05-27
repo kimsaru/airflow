398125ca2705ccb221015752dd356cc70d84bec7
kimsaru <kimsaru@gmail.com>
2025-05-27 12:36:21 +0900

    xcom 테스트1

---
+++ dags/dags_python_with_xcom_eg2.py
@@ -0,0 +1,32 @@
+import datetime
+import pendulum
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.decorators import dag, task
+
+with DAG(
+    dag_id="dags_python_with_xcom_eg2",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2025, 5, 1, tz="Asia/Seoul"),
+    tags=["testtest1"],
+    catchup=False
+) as dag:
+    
+    @task(task_id="python_xcom_push_by_return")
+    def xcom_push_result(**kwargs):
+        return 'Success'
+
+    @task(task_id="python_xcom_pull_1")
+    def xcom_pull_1(**kwargs):
+        ti = kwargs['ti']
+        v1 = ti.xcom_pull(task_ids='python_xcom_push_by_return')
+        print('xcom_pull 메서드로 직접 찾은 리턴 값:' + v1)
+
+    @task(task_id="python_xcom_pull_2")
+    def xcom_pull_2(status, **kwargs):
+        print('함수 입력값으로 받은 값: ' + status)
+
+
+    xcom_push_result() >> xcom_pull_1()
+    xcom_pull_2(xcom_push_result())
