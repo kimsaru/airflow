b52a058e1ff6e2817606e0fd25ba68f025170171
kimsaru <kimsaru@gmail.com>
2025-05-29 11:23:12 +0900

    triggerer test

---
+++ dags/dags_time_sensor with_async.py
@@ -0,0 +1,19 @@
+from airflow.models.dag import DAG
+from airflow.sensors.date_time import DateTimeSensorAsync
+
+import datetime
+import pendulum
+
+with DAG(
+    dag_id="dags_time_sensor with_async",
+    schedule="*/10 * * * *",
+    start_date=pendulum.datetime(2025, 5, 26, 0, 0, 0),
+    end_date=pendulum.datetime(2025, 5, 26, 1, 0, 0),
+    catchup=True,
+    tags=["testtest1"]
+) as dag:
+    
+    async_sensor = DateTimeSensorAsync(
+        task_id = "async_sensor",
+        target_time=""" {{ macros.datetime.utcnow() + macros.timedelta(minutes=5)}}"""
+    )
---
+++ dags/dags_time_sensor.py
@@ -0,0 +1,19 @@
+from airflow.models.dag import DAG
+from airflow.sensors.date_time import DateTimeSensor
+
+import datetime
+import pendulum
+
+with DAG(
+    dag_id="dags_time_sensor",
+    schedule="*/10 * * * *",
+    start_date=pendulum.datetime(2025, 5, 26, 0, 0, 0),
+    end_date=pendulum.datetime(2025, 5, 26, 1, 0, 0),
+    catchup=False,
+    tags=["testtest1"]
+) as dag:
+    
+    sync_sensor = DateTimeSensor(
+        task_id = "sync_sensor",
+        target_time=""" {{ macros.datetime.utcnow() + macros.timedelta(minutes=5)}}"""
+    )
