df13e8a3daad2110287656f6f98accd4606617a5
kimsaru <kimsaru@gmail.com>
2025-05-30 13:43:41 +0900

    test_20250530_1

---
+++ dags/dags_gcp_operator_1.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+from airflow.providers.google.cloud.operators.bigquery import (
+    BigQueryInsertJobOperator,
+    BigQueryGetDataOperator,
+)
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+
+
+def get_query_for_yesterday_partition() -> str:
+    return """
+        SELECT id, player1_id, battle_date FROM `gcloud_yj.basic.battle` WHERE battle_date = PARSE_DATE('%Y-%m-%d', '2023-07-16')
+    """
+
+with DAG(
+    dag_id="dags_gcp_operator",
+    start_date=days_ago(1),
+    schedule_interval=None,
+    catchup=False,
+    tags=["bigquery"],
+) as dag:
+
+    overwrite_partition = BigQueryInsertJobOperator(
+        task_id="overwrite_partition_query_only",
+        configuration={
+            "query": {
+                "query": get_query_for_yesterday_partition(),
+                "destinationTable": {
+                    "projectId": "gcloud_yj",
+                    "datasetId": "basic",
+                    "tableId": "test_by_partition"
+                },
+                "writeDisposition": "WRITE_TRUNCATE",
+                "createDisposition": "CREATE_NEVER",
+                "useLegacySql": False
+            }
+        },
+        location="US"
+    )
