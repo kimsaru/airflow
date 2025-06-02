6b16ed41014bf9690eb013395fb0bca76b91d30e
kimsaru <kimsaru@gmail.com>
2025-06-02 14:23:41 +0900

    bigquery test3

--- dags/dags_gcp_operator_3.py
+++ dags/dags_gcp_operator_3.py
@@ -1,5 +1,6 @@
 from airflow import DAG
-from airflow.providers.google.cloud.operators.bigquery import BigQueryExecuteQueryOperator, BigQueryToGCSOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryExecuteQueryOperator
+from airflow.providers.google.cloud.transfers.bigquery_to_gcs import BigQueryToGCSOperator
 from airflow.utils.dates import days_ago
 
 with DAG(
@@ -19,19 +20,19 @@ with DAG(
         use_legacy_sql=False,
         destination_dataset_table='gcloud-yj.basic.test_by_partition_temp1', 
         write_disposition='WRITE_TRUNCATE',
-        gcp_conn_id='google_cloud_default',
+        gcp_conn_id='airflow_bigquery_test',
         location='US',
     )
 
     # 2. 쿼리 결과를 GCS로 export
     export_task = BigQueryToGCSOperator(
-        task_id='export_to_gcs',
+        task_id='export_task',
         source_project_dataset_table='gcloud-yj.basic.test_by_partition_temp1',
         destination_cloud_storage_uris=['gs://jin_com/airflow/output1.csv'],  
         export_format='CSV',
         field_delimiter=',',
         print_header=True,
-        gcp_conn_id='google_cloud_default',
+        gcp_conn_id='airflow_bigquery_test',
     )
 
     query_task >> export_task
