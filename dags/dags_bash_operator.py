398cbf090c62aa65d23e708e7a34c2292e7e9e91
kimsaru <kimsaru@gmail.com>
2025-06-12 21:08:00 +0900

    Revert "test1"
    This reverts commit cbcd56afba4c52c4095b7326da44e89f894ef791.

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -25,9 +25,4 @@ with DAG(
         bash_command="echo $HOSTNAME",
     )
 
-    bash_t3 = BashOperator(
-        task_id="bash_t3",
-        bash_command="echo $HOSTNAME",
-    )
-
-    bash_t1 >> bash_t2 >> bash_t3
+    bash_t1 >> bash_t2
