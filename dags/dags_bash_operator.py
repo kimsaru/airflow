47fce67f49fae169d75d499770ddf8e1b864b863
kimsaru <kimsaru@gmail.com>
2025-06-12 21:41:23 +0900

    git test

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -25,4 +25,9 @@ with DAG(
         bash_command="echo $HOSTNAME",
     )
 
-    bash_t1 >> bash_t2
+    bash_t3 = BashOperator(
+        task_id="bash_t3",
+        bash_command="echo $HOSTNAME",
+    )
+
+    bash_t1 >> bash_t2 >> bash_t3
