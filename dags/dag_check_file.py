49c217cd40e3fb3a64b6d013aa9d3fa86761a137
kimsaru <kimsaru@gmail.com>
2025-06-14 18:38:05 +0900

    test

---
+++ dags/dag_check_file.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.utils.dates import days_ago
+
+def check_file_exists(response):
+    json_data = response.json()
+    return json_data.get("exists", False) is True
+
+with DAG(
+    dag_id='check_file_api_sensor',
+    start_date=days_ago(1),
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+
+    wait_file = HttpSensor(
+        task_id='wait_file',
+        http_conn_id='my_api_server',
+        endpoint='check_file/file1.csv',  # 확인할 파일명
+        response_check=check_file_exists,
+        poke_interval=10,
+        timeout=60,
+        mode='reschedule',
+    )
