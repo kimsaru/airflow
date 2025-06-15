6e212a2083edc1cdbd240eb2c9bb83da5377dc3c
kimsaru <kimsaru@gmail.com>
2025-06-15 20:34:42 +0900

    test

--- dags/loader_multi_yaml.py
+++ dags/loader_multi_yaml.py
@@ -2,27 +2,29 @@ import yaml
 import os
 from airflow import DAG
 from airflow.utils.module_loading import import_string
-from datetime import datetime
+from datetime import datetime, date
 
+# 안전하게 start_date 처리
+def parse_start_date(val):
+    if isinstance(val, datetime):
+        return val
+    if isinstance(val, date):
+        return datetime(val.year, val.month, val.day)
+    if isinstance(val, str):
+        try:
+            return datetime.fromisoformat(val)
+        except ValueError:
+            return datetime.strptime(val, "%Y-%m-%d")
+    raise ValueError(f"Invalid start_date: {val} (type: {type(val)})")
+
+# 단일 YAML 파일을 DAG 객체로 변환
 def load_yaml_dag(yaml_path):
-    with open(yaml_path) as f:
+    with open(yaml_path, "r") as f:
         config = yaml.safe_load(f)
 
     dag_config = config['dag']
-
-    def parse_start_date(val):
-        # val이 이미 datetime 또는 date 타입이면 datetime으로 변환 후 반환
-        if isinstance(val, datetime):
-            return val
-        if isinstance(val, date):
-            return datetime(val.year, val.month, val.day)
-        # 문자열이면 fromisoformat 사용
-        if isinstance(val, str):
-            return datetime.fromisoformat(val)
-        raise ValueError(f"Cannot parse start_date: {val}")
-
-    # 기존 코드에서 start_date 부분만 변경
     start_date = parse_start_date(dag_config['start_date'])
+
     dag = DAG(
         dag_id=dag_config['dag_id'],
         start_date=start_date,
@@ -43,6 +45,7 @@ def load_yaml_dag(yaml_path):
 
     return dag
 
+# YAML 파일 여러 개 읽어서 각각 DAG 등록
 yaml_dir = os.path.join(os.path.dirname(__file__), "yaml_dags")
 for filename in os.listdir(yaml_dir):
     if filename.endswith(".yaml"):
