b1bad2eed6f47662d4ae63115143c03c386f7975
kimsaru <kimsaru@gmail.com>
2025-06-15 16:39:20 +0900

    test

---
+++ dags/dags_python_show_templates_4.py
@@ -0,0 +1,23 @@
+import datetime
+import pendulum
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.decorators import dag, task
+
+with DAG(
+    dag_id="dags_python_show_templates_3",
+    schedule="30 18 * * *",
+    start_date=pendulum.datetime(2025, 6, 14, tz="Asia/Seoul"),
+    tags=["testtest1"],
+    catchup=False
+) as dag:
+    
+    @task(task_id="print_task")
+    def show_tempates(**kwargs):
+        from pprint import pprint
+        pprint(kwargs)
+
+    print_task = show_tempates()
+
+    print_task
