ed45c7a71dffaa9a2fb8c16c66e4d047605d89f8
kimsaru <kimsaru@gmail.com>
2025-06-16 19:27:11 +0900

    a

---
+++ dags/http_sensor_trigger_rule_test_1.py
@@ -0,0 +1,43 @@
+from airflow import DAG
+from airflow.sensors.http_sensor import HttpSensor
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+from airflow.utils.trigger_rule import TriggerRule
+
+default_args = {
+    'owner': 'airflow',
+}
+
+with DAG(
+    dag_id='http_sensor_trigger_rule_test_1',
+    default_args=default_args,
+    start_date=days_ago(1),
+    schedule_interval=None,
+    tags=["testtest2"],
+    catchup=False
+) as dag:
+
+    start = DummyOperator(task_id='start')
+
+    # 실패를 유도하기 위해 존재하지 않는 endpoint 사용
+    failing_http_sensor = HttpSensor(
+        task_id='failing_http_sensor',
+        http_conn_id='non_existent_connection',  # 없는 커넥션 ID
+        endpoint='fake/endpoint',
+        method='GET',
+        response_check=lambda response: "OK" in response.text,
+        poke_interval=5,
+        timeout=10,
+        mode='poke',
+        soft_fail=False  # 실패를 DAG 실패로 반영 (중요!)
+    )
+
+    task_all_done = BashOperator(
+        task_id='task_all_done',
+        bash_command='echo "Runs regardless of upstream result"',
+        trigger_rule=TriggerRule.ALL_DONE
+    )
+
+    # DAG 구조
+    start >> failing_http_sensor >> task_all_done
