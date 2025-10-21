1ad0d5b769c37fdc5f9346ab820b3a67970eca67
kimsaru <kimsaru@gmail.com>
2025-10-21 20:04:33 +0900

    basic

---
+++ dags/basic.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+def print_time():
+    print("This runs every 10 minutes!")
+
+with DAG(
+    dag_id="basic",
+    start_date=datetime(2023, 1, 1),
+    schedule_interval="*/10 * * * *",  # 10분마다
+    catchup=False,
+    tags=["example"],
+) as dag:
+
+    task = PythonOperator(
+        task_id="print_task",
+        python_callable=print_time,
+    )
