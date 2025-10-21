35ceb4a58115aefa8f8240f52c1f872190feaf5b
kimsaru <kimsaru@gmail.com>
2025-10-21 20:39:10 +0900

    basic1

--- dags/basic.py
+++ dags/basic.py
@@ -8,7 +8,7 @@ def print_time():
 with DAG(
     dag_id="basic",
     start_date=datetime(2023, 1, 1),
-    schedule_interval="*/10 * * * *",  # 10분마다
+    schedule_interval="50 * * * *",  # 10분마다
     catchup=False,
     tags=["example"],
 ) as dag:
