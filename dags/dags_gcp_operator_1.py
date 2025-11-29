a94acae089a9dcc013db0fff7c841252f37d3180
kimsaru <kimsaru@gmail.com>
2025-11-29 12:44:51 +0900

    'test'

--- dags/dags_gcp_operator_1.py
+++ dags/dags_gcp_operator_1.py
@@ -1,17 +1,19 @@
 from airflow import DAG
-from airflow.providers.google.cloud.operators.bigquery import (
-    BigQueryInsertJobOperator,
-    BigQueryGetDataOperator,
-)
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
+from airflow.operators.python import PythonOperator
+from airflow.providers.google.common.hooks.base_google import GoogleBaseHook
 
+def print_gcp_credentials():
+    hook = GoogleBaseHook(gcp_conn_id="airflow_bigquery_test")
+    credentials = hook.get_credentials()
+    project_id = hook.project_id
 
-def get_query_for_yesterday_partition() -> str:
-    return """
-        SELECT id, player1_id, battle_date as event_date FROM `gcloud-yj.basic.battle` 
-        WHERE battle_date between PARSE_DATE('%Y-%m-%d', '2023-07-15') and PARSE_DATE('%Y-%m-%d', '2023-07-16')
-    """
+    print("Project ID:", project_id)
+    print("Credentials type:", type(credentials))
+    # service account info 확인 가능
+    if hasattr(credentials, "service_account_email"):
+        print("Service account email:", credentials.service_account_email)
 
 with DAG(
     dag_id="dags_gcp_operator_1",
@@ -21,21 +23,7 @@ with DAG(
     tags=["bigquery"],
 ) as dag:
 
-    overwrite_partition = BigQueryInsertJobOperator(
-        task_id="overwrite_partition_query_only",
-        configuration={
-            "query": {
-                "query": get_query_for_yesterday_partition(),
-                "destinationTable": {
-                    "projectId": "gcloud-yj",
-                    "datasetId": "basic",
-                    "tableId": "test_by_partition"
-                },
-                "writeDisposition": "WRITE_TRUNCATE",
-                "createDisposition": "CREATE_NEVER",
-                "useLegacySql": False
-            }
-        },
-        location="US",
-        gcp_conn_id="airflow_bigquery_test",
+    test_task = PythonOperator(
+        task_id="print_gcp_credentials",
+        python_callable=print_gcp_credentials,
     )
