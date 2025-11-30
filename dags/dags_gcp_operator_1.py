42831c6224d7390666b5923c372d70d42f96c2a6
kimsaru <kimsaru@gmail.com>
2025-11-30 13:36:35 +0900

    test

--- dags/dags_gcp_operator_1.py
+++ dags/dags_gcp_operator_1.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
 from datetime import datetime
 from google.cloud import bigquery
 from airflow.providers.google.common.hooks.base_google import GoogleBaseHook
@@ -13,23 +14,23 @@ def generate_insert_sql_chunks():
     200개 이상 SQL 생성 → chunk_size=100 으로 나누어
     Dynamic Task Mapping에서 task가 2개 생성되도록 함
     """
-    countries = [f"country_{i}" for i in range(20)]  # 20개 나라
+    countries = [f"country_{i}" for i in range(10)]  # 20개 나라
     tests = [f"test_{i}" for i in range(11)]        # 10개 테스트
 
-    # 20 × 10 = 200개 SQL 생성됨
+    # 10 × 11 = 200개 SQL 생성됨
     all_sql = []
 
     for c in countries:
         for t in tests:
             sql = f"""
             INSERT INTO `basic.my_table` 
-            (country, test_name, created_at)
-            VALUES ('{c}', '{t}', CURRENT_TIMESTAMP())
+            (country, test_name, created_at, is_exist)
+            VALUES ('{c}', '{t}', CURRENT_TIMESTAMP(), 'n')
             """
             all_sql.append(sql)
 
     # 100개씩 chunk
-    chunk_size = 100
+    chunk_size = 50
     results = [all_sql[i:i + chunk_size] for i in range(0, len(all_sql), chunk_size)]
 
     print(f"총 SQL 개수: {len(all_sql)}")
@@ -67,6 +68,9 @@ with DAG(
     catchup=False,
 ) as dag:
 
+    # Start
+    start = EmptyOperator(task_id="start")
+
     # SQL 생성 Task
     generate_sql_task = PythonOperator(
         task_id="generate_sql_chunks",
@@ -74,9 +78,15 @@ with DAG(
     )
 
     # Dynamic Task Mapping으로 SQL chunk 실행
-    PythonOperator.partial(
+    execute_chunks = PythonOperator.partial(
         task_id="execute_sql_chunk",
         python_callable=execute_sql_chunk
     ).expand(
         op_kwargs=generate_sql_task.output.map(lambda r: {"row": r})
     )
+
+    # End
+    end = EmptyOperator(task_id="end")
+
+    # Task Flow
+    start >> generate_sql_task >> execute_chunks >> end
